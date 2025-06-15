8d851e6a4b5551bac6b6f79a22579020f9724234
Alexander Kvapel <alex.kvapel@gmail.com>
2025-06-16 01:28:55 +0300

    menu button script modified

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,22 +4,28 @@ const menuItem = document.querySelectorAll('button[data-scroll-to^="#"]');
 const main = document.querySelector('.main');
 
 menuButton.addEventListener('click', function () {
+  const menuButtonSVG = document.querySelector('.menu-button img');
+
   if (menu.style.display == 'block') {
     menu.style.display = 'none';
     main.style.display = 'block';
+    menuButtonSVG.setAttribute('src', './images/burger-closed.svg');
   } else {
     menu.style.display = 'block';
     main.style.display = 'none';
+    menuButtonSVG.setAttribute('src', './images/burger-opened.svg');
   }
 });
 
 window.addEventListener('resize', function () {
+  const menuButtonSVG = document.querySelector('.menu-button img');
   const currentWidth = this.innerWidth;
   const breakpoint = 920;
 
   if (currentWidth >= breakpoint) {
     menu.style.display = 'block';
     main.style.display = 'block';
+    menuButtonSVG.setAttribute('src', './images/burger-closed.svg');
   } else if (currentWidth < breakpoint) {
     menu.style.display = 'none';
     main.style.display = 'block';
